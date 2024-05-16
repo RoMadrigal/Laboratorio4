ad356ff2330d15a9e01b9484ae556c471cbe57a2
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-05-15 21:48:19 -0600

    Define el arrego y realiza busqueda lineal

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,4 +1,18 @@
 
 #include <stdio.h>
 
+int arreglo[] = {2, 4, 6, 23, 56, 79};
+
+#define ARR_SIZE (sizeof(arreglo) / sizeof(arreglo[0]))
+
+
+int busquedaLineal(int valor) {
+    for (int i = 0; i < ARR_SIZE; i++) {
+        if (arreglo[i] == valor) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 
