3a118ba965007f9c8ce63b40ba92943599e83505
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-05-17 21:03:09 -0600

    Agrego funcion encontrarMinMax

--- ejercicio2.c
+++ ejercicio2.c
@@ -30,3 +30,18 @@ int encontrarMax(int arr[], int longitud) {
 }
 
 
+
+void encontrarMinMax(int *arreglo, int *minimo, int *maximo, int longitud) {
+    *minimo = arreglo[0];
+    *maximo = arreglo[0];
+    for (int i = 1; i < longitud; i++) {
+        if (arreglo[i] < *minimo) {
+            *minimo = arreglo[i];
+        }
+        if (arreglo[i] > *maximo) {
+            *maximo = arreglo[i];
+        }
+    }
+}
+
+
