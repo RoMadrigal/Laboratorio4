d93a6d6428b7559af74bff3340cb22cad81f1a8f
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-05-17 21:00:26 -0600

    Agrego funciones para encontrar maximo y minimo

--- ejercicio2.c
+++ ejercicio2.c
@@ -5,3 +5,28 @@ int arreglo[] = {21, 24, 65, 3, 56, 12, 35, 15};
 
 #define ARR_SIZE (sizeof(arreglo) / sizeof(arreglo[0]))
 
+
+
+int encontrarMin(int arr[], int longitud) {
+    int minimo = arr[0];
+    for (int i = 1; i < longitud; i++) {
+        if (arr[i] < minimo) {
+            minimo = arr[i];
+        }
+    }
+    return minimo;
+}
+
+
+
+int encontrarMax(int arr[], int longitud) {
+    int maximo = arr[0];
+    for (int i = 1; i < longitud; i++) {
+        if (arr[i] > maximo) {
+            maximo = arr[i];
+        }
+    }
+    return maximo;
+}
+
+
